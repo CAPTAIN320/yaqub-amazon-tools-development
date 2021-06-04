0d9203a011d55fef0849b778de212745c3729d90
captain320 <yaqub320@gmail.com>
2021-06-05 00:21:48 +0900

    added files

--- coding.py
+++ coding.py
@@ -1,2 +1,24 @@
-import os
+import pandas as pd
+
+
+top_categories_array = ["Appliances", "Beauty & Personal Care", "Clothing, Shoes & Jewelry", 
+                        "Electronics", "Handmade", "Sports & Outdoors", "Tools & Home Improvement",
+                        "Toys & Games"]
+
+top_categories_url_dict = {""}
+
+all_categories_array = [""]
+
+top_brands_dict = {
+
+}
+
+df_csv_file = pd.read_csv("categories\Brands - " + top_categories_array[2] + ".csv")
+
+#csv_file = open("categories\Brands - " + top_categories_array[0] + ".csv", encoding="utf-8")
+
+
+print(df_csv_file[df_csv_file.brand != None])
+
+
 
