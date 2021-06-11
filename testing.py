aa02cc8fb9eee296d6013779009aadd1c8f421d9
captain320 <yaqub320@gmail.com>
2021-06-11 12:06:13 +0900

    update

--- testing.py
+++ testing.py
@@ -29,7 +29,7 @@ b = requests.get('https://www.amazon.com/shops/HAPPYSASHIMI?&marketplaceID=ATVPD
 print(b)
 print(b.url)
  """
-
+""" 
 # set up the request parameters
 params = {
   'api_key': 'BDA72F7899D94417B5ABB1B78E5E59C9',
@@ -44,3 +44,19 @@ api_result = requests.get('https://api.rainforestapi.com/request', params)
 
 # print the JSON response from Rainforest API
 print(json.dumps(api_result.json()))
+ """
+# set up the request parameters
+params = {
+  'api_key': 'BDA72F7899D94417B5ABB1B78E5E59C9',
+  'type': 'product',
+  'amazon_domain': 'amazon.com',
+  'asin': 'B073JYC4XM'
+}
+
+# make the http GET request to Rainforest API
+api_result = requests.get('https://api.rainforestapi.com/request', params)
+
+# print the JSON response from Rainforest API
+print(json.dumps(api_result.json()))
+
+
