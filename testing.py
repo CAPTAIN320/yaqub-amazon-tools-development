f3cb7c80917b63780b2ad6e66f22f312ef69cc29
captain320 <yaqub320@gmail.com>
2021-06-14 20:47:58 +0900

    last change

--- testing.py
+++ testing.py
@@ -5,15 +5,6 @@ import tempfile
 import requests
 import json
 
-with urllib.request.urlopen('http://python.org/') as response:
-    with tempfile.NamedTemporaryFile(delete=False) as tmp_file:
-        shutil.copyfileobj(response, tmp_file)
-
-with open(tmp_file.name) as html:
-    pass
-
-
-
 """ 
 r = requests.get('https://www.amazon.com/shops/HAPPYSASHIMI')
 print(r)
@@ -24,27 +15,14 @@ a = requests.get('https://www.amazon.com/gp/shops/HAPPYSASHIMI?ie=UTF8&%2AVersio
 print(a)
 print(a.url)
  """
-""" 
-b = requests.get('https://www.amazon.com/shops/HAPPYSASHIMI?&marketplaceID=ATVPDKIKX0DER')
+
+b = requests.get('https://www.amazon.com/shops/holidaygiftsexpress?&marketplaceID=ATVPDKIKX0DER')
 print(b)
 print(b.url)
- """
+
+
+
 """ 
-# set up the request parameters
-params = {
-  'api_key': 'BDA72F7899D94417B5ABB1B78E5E59C9',
-  'type': 'seller_profile',
-  'amazon_domain': 'amazon.com',
-  'output': 'json',
-  'seller_id': 'A02211013Q5HP3OMSZC7W'
-}
-
-# make the http GET request to Rainforest API
-api_result = requests.get('https://api.rainforestapi.com/request', params)
-
-# print the JSON response from Rainforest API
-print(json.dumps(api_result.json()))
- """
 # set up the request parameters
 params = {
   'api_key': 'BDA72F7899D94417B5ABB1B78E5E59C9',
@@ -58,5 +36,4 @@ api_result = requests.get('https://api.rainforestapi.com/request', params)
 
 # print the JSON response from Rainforest API
 print(json.dumps(api_result.json()))
-
-
+ """
